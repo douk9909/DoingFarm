830a3d8486df0df93a60b4c6dbda972e54b40801
douk9909 <douk9909@gmail.com>
2026-04-18 16:12:32 +0900

    feat: 로그인 및 회원가입 라우트 그룹 추가

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,8 @@
+export default function LoginPage() {
+  return (
+    <section>
+      <h1>로그인</h1>
+      <p>로그인 페이지 초안</p>
+    </section>
+  );
+}
---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,8 @@
+export default function SignupPage() {
+  return (
+    <section>
+      <h1>회원가입</h1>
+      <p>회원가입 페이지 초안</p>
+    </section>
+  );
+}
