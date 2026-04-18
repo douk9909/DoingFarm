79b5cf343cc97e8b2efa0cafa4e1843c6419e3d5
douk9909 <douk9909@gmail.com>
2026-04-18 16:23:07 +0900

    feat: 공통 Button과 Input 컴포넌트 추가

---
+++ src/components/common/button/Button.tsx
@@ -0,0 +1,2 @@
+import styles from './Button.module.css';
+import { cn } from '@/lib/utils/cn';
---
+++ src/components/common/input/Input.tsx
@@ -0,0 +1,11 @@
+import styles from './Input.module.css';
+import { cn } from '@/lib/utils/cn';
+
+export default function Input({
+
+}) {
+  return (
+    <div>
+    </div>
+  );
+}
