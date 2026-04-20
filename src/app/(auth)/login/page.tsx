775c9bb29cc89eb5aa0192b897733501634747bb
JuHeonParkk <ju10913@naver.com>
2026-04-20 17:03:33 +0900

    feat: input type 설계 작업

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,8 +1,12 @@
+import { Input } from '../../../components/common/input/Input';
+
 export default function LoginPage() {
   return (
     <section>
       <h1>로그인</h1>
       <p>로그인 페이지 초안</p>
+      <Input placeholder="아이디" />
+      <Input placeholder="비밀번호" type="password" />
     </section>
   );
 }
--- src/components/common/input/Input.tsx
+++ src/components/common/input/Input.tsx
@@ -1,12 +1,38 @@
+import { ComponentType, InputHTMLAttributes, forwardRef } from 'react';
 import styles from './Input.module.css';
 import { cn } from '@/lib/utils/cn';
 
-function Input() {
+type IconType = ComponentType<{ size?: number; color?: string }>;
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  leftIcon?: IconType;
+  rightIcon?: IconType;
+  onRightClick?: () => void;
+
+  limit?: number;
+  errorMsg?: string;
+}
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(function Input(
+  { leftIcon: LeftIcon, rightIcon: RightIcon, onRightClick, limit, errorMsg, ...props }: InputProps,
+  ref,
+) {
   return (
-    <div className={styles.container}>
-      <div className={styles.iconWrapper}></div>
-      <input className={styles.inputStyle} />
-      <div className={styles.iconWrapper}></div>
+    <div className={styles.wrapper}>
+      <div className={styles.inputContainer}>
+        {LeftIcon && (
+          <div className={styles.iconWrapper}>
+            <LeftIcon size={20} />
+          </div>
+        )}
+        <input className={styles.inputStyle} {...props} ref={ref} />
+        {RightIcon && (
+          <div className={styles.iconWrapper} onClick={onRightClick}>
+            <RightIcon size={24} />
+          </div>
+        )}
+      </div>
+      {errorMsg && <p className={styles.errorMsg}>{errorMsg}</p>}
     </div>
   );
-}
+});
