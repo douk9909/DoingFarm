70d04bb843b768d3886b0beee307f4304d6c3ef2
gnath <rudqor1618@naver.com>
2026-04-21 14:53:35 +0900

    feat: 로그인 폼 UI 구현
    - 이메일/비밀번호 입력 필드 구현
    - 비밀번호 표시/숨김 토글
    - 이메일 유효성 검사 및 에러 메시지
    - 로고 클릭 시 홈으로 이동
    - 회원가입 링크 연결
    - 반응형 레이아웃 (모바일 퍼스트)

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,7 +5,6 @@ import Image from 'next/image';
 import loginBg from '@/assets/backgroundImg/login_bg.svg';
 import loginIllust from '@/assets/backgroundImg/login.svg';
 
-
 export default function LoginPage() {
   return (
     <div className={styles.container} style={{ backgroundImage: `url(${loginBg.src})` }}>
@@ -15,12 +14,7 @@ export default function LoginPage() {
         </section>
 
         <section className={styles.illustrationSection}>
-          <Image
-            src={loginIllust}
-            alt="로그인 일러스트"
-            width={823}
-            height={823}
-          />
+          <Image src={loginIllust} alt="로그인 일러스트" width={823} height={823} />
         </section>
       </main>
     </div>
