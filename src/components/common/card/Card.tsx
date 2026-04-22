3342a707dc6b415690bdf60be5ca5bf965e4caa1
JuHeonParkk <ju10913@naver.com>
2026-04-23 01:14:13 +0900

    feat: 공통 컴포넌트 card UI 구현 및 스타일링

---
+++ src/components/common/card/Card.tsx
@@ -0,0 +1,42 @@
+import Avatar from '@/components/common/avatar/Avatar';
+
+import styles from './Card.module.css';
+
+interface CardProps {
+  src?: string | null;
+  title: string;
+  tags?: string[];
+  date: string;
+  authorName: string;
+}
+
+export default function Card({ src, title, tags, date, authorName }: CardProps) {
+  return (
+    <div className={styles.cardContainer}>
+      {src && (
+        <div className={styles.imageWrapper}>
+          <img src={src} alt={title} className={styles.cardImage} />
+        </div>
+      )}
+
+      <div className={styles.cardContent}>
+        <h3 className={styles.cardTitle}>{title}</h3>
+
+        <div className={styles.cardTags}>
+          {tags?.map((tag) => (
+            <span key={tag} className={styles.cardTag}>
+              {tag}
+            </span>
+          ))}
+        </div>
+
+        <p className={styles.cardDate}>{date}</p>
+        <div className={styles.cardAuthor}>
+          {/* Todo: Avater 컴포넌트 머지 시 name={authorName} 및 size={20} */}
+          <Avatar />
+          <span className={styles.cardAuthorName}>{authorName}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
