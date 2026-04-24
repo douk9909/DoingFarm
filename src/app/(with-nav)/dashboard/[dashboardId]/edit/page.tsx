2ae176abe4a12c3dfc3a56d394bd69013d74b123
JuHeonParkk <ju10913@naver.com>
2026-04-25 02:02:56 +0900

    feat: 대시보드 수정 페이지 UI 구조 작업

--- src/app/(with-nav)/dashboard/[dashboardId]/edit/page.tsx
+++ src/app/(with-nav)/dashboard/[dashboardId]/edit/page.tsx
@@ -1,3 +1,7 @@
+import Avatar from '@/components/common/avatar/Avatar';
+import Button from '@/components/common/button/Button';
+import { Input } from '@/components/common/input/Input';
+
 interface DashboardEditPageProps {
   params: Promise<{
     dashboardId: string;
@@ -9,8 +13,64 @@ export default async function DashboardEditPage({ params }: DashboardEditPagePro
 
   return (
     <section>
-      <h1>대시보드 수정</h1>
-      <p>dashboardId: {dashboardId}</p>
+      <button>돌아가기</button>
+
+      {/* 대시보드 이름 변경 */}
+      <div>
+        <h1>대시보드</h1>
+        <Input type="text" label="대시보드 이름" value={dashboardId} />
+        <div>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+          <button></button>
+        </div>
+        <Button>변경</Button>
+      </div>
+      {/* 대시보드 구성원 변경 */}
+      <div>
+        <div>
+          <h1>구성원</h1>
+          <div>
+            <span>n 페이지 중 n</span>
+            <div>
+              <button></button>
+              <button></button>
+            </div>
+          </div>
+        </div>
+        <div>
+          <p>이름</p>
+          <ul>
+            <li>
+              <Avatar name="이름" />
+              <span>이름</span>
+              <Button>삭제</Button>
+            </li>
+          </ul>
+        </div>
+      </div>
+      {/* 대시보드 초대 내역 */}
+      <div>
+        <div>
+          <h1>초대 내역</h1>
+          <Button>초대하기</Button>
+        </div>
+        <div>
+          <p>이메일</p>
+          <ul>
+            <li>
+              <Avatar name="이름" />
+              <span>이메일</span>
+              <Button>취소</Button>
+            </li>
+          </ul>
+        </div>
+      </div>
+      {/* 대시보드 삭제 버튼 */}
+      <Button>대시보드 삭제 </Button>
     </section>
   );
 }
