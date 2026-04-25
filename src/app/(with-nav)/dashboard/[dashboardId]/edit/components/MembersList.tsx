2880342a3b26026ce6556f9592bcffd51f813c3b
JuHeonParkk <ju10913@naver.com>
2026-04-25 15:52:59 +0900

    feat: 구성원 삭제 및 초대 이메일 삭제 기능 구현

--- src/app/(with-nav)/dashboard/[dashboardId]/edit/components/MembersList.tsx
+++ src/app/(with-nav)/dashboard/[dashboardId]/edit/components/MembersList.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { useState } from 'react';
+
 import Avatar from '@/components/common/avatar/Avatar';
 import Button from '@/components/common/button/Button';
 
@@ -6,6 +10,26 @@ import ArrowLeftIcon from '@/assets/icons/ArrowLeftIcon';
 import ArrowRightIcon from '@/assets/icons/ArrowRightIcon';
 
 export default function MembersList() {
+  // Todo: API 연결 후 제거
+  const mockData = [
+    { id: 1, userId: 101, nickname: '제나', profileImageUrl: null, isOwner: true },
+    { id: 2, userId: 102, nickname: '사용자1', profileImageUrl: null, isOwner: false },
+    { id: 3, userId: 103, nickname: '사용자2', profileImageUrl: null, isOwner: false },
+  ];
+
+  const [members, setMembers] = useState(mockData);
+  // Todo: 페이지네이션 추가
+
+  const handleDeleteMember = (id: number, nickname: string) => {
+    if (confirm(`${nickname} 님을 삭제하시겠습니까?`)) {
+      setMembers((prev) => prev.filter((member) => member.id !== id));
+      // Todo: API 연결
+    }
+
+    // Todo: 토스트 띄우기
+    alert('삭제되었습니다');
+  };
+
   return (
     <div className={styles.section}>
       <div className={styles.sectionHeader}>
@@ -25,15 +49,21 @@ export default function MembersList() {
       <div className={styles.sectionContent}>
         <p className={styles.subTitle}>이름</p>
         <ul className={styles.list}>
-          <li className={styles.item}>
-            <div className={styles.profileWrapper}>
-              <Avatar name="이름" />
-              <span className={styles.profileName}>이름</span>
-            </div>
-            <Button variant="secondary" className={styles.buttonStyle}>
-              삭제
-            </Button>
-          </li>
+          {members.map((member) => (
+            <li key={member.id} className={styles.item}>
+              <div className={styles.profileWrapper}>
+                <Avatar src={member.profileImageUrl} name={member.nickname} />
+                <span className={styles.profileName}>{member.nickname}</span>
+              </div>
+              <Button
+                variant="secondary"
+                onClick={() => handleDeleteMember(member.id, member.nickname)}
+                className={styles.buttonStyle}
+              >
+                삭제
+              </Button>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
