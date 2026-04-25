0acae57cdbb8bad8a73d906cb2f887172eb1008f
JuHeonParkk <ju10913@naver.com>
2026-04-25 15:37:28 +0900

    feat: colorPicker 공통 컴포넌트로 분리

--- src/app/(with-nav)/dashboard/[dashboardId]/edit/page.tsx
+++ src/app/(with-nav)/dashboard/[dashboardId]/edit/page.tsx
@@ -2,8 +2,9 @@ import EditForm from './components/EditForm';
 import MembersList from './components/MembersList';
 import InvitationsList from './components/InvitationsList';
 
+import { DASHBOARD_COLORS } from '@/lib/constants/color';
+
 import styles from './edit.module.css';
-import TrashFillIcon from '@/assets/icons/TrashFillIcon';
 
 interface DashboardEditPageProps {
   params: Promise<{
@@ -14,13 +15,20 @@ interface DashboardEditPageProps {
 export default async function DashboardEditPage({ params }: DashboardEditPageProps) {
   const { dashboardId } = await params;
 
+  // Todo: API 연결 후 mock data삭제
+  const initialData = {
+    id: '1',
+    title: '테스트 프로젝트',
+    color: DASHBOARD_COLORS[0], // ColorPicker 팔레트에 있는 값 중 하나
+  };
+
   return (
     <section className={styles.container}>
       <button className={styles.prevButton}>돌아가기</button>
 
       <div className={styles.contentWrapper}>
         {/* 대시보드 이름 변경 */}
-        <EditForm initialTitle="테스트 " initialColor="색상" />
+        <EditForm initialTitle={initialData.title} initialColor={initialData.color} />
         {/* 대시보드 구성원 변경 */}
         <MembersList />
         {/* 대시보드 초대 내역 */}
