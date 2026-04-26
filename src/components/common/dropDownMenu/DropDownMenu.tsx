6b95b7307016a47ee21e62e1585fa2a184357d5d
JuHeonParkk <ju10913@naver.com>
2026-04-26 15:07:58 +0900

    feat: dropDownMenu UI 구조 및 타입 선언

---
+++ src/components/common/dropDownMenu/DropDownMenu.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { useState, useRef, useEffect } from 'react';
+
+import MoreIcon from '@/assets/icons/MoreIcon';
+import SettingIcon from '@/assets/icons/SettingIcon';
+
+import styles from './dropDownMenu.module.css';
+
+interface dropDownMenuProps {
+  status: 'setting' | 'kebab';
+  menuItems: Array<{
+    label: string;
+    onClick: () => void;
+  }>;
+}
+
+const ICON_MAP = {
+  setting: SettingIcon,
+  kebab: MoreIcon,
+};
+
+export default function DropDownMenu({ status, menuItems }: dropDownMenuProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  const Icon = ICON_MAP[status];
+
+  return (
+    <div className={styles.container} ref={menuRef}>
+      <button type="button" className={styles.iconButton} onClick={() => setIsOpen(!isOpen)}>
+        <Icon />
+      </button>
+      {isOpen && (
+        <ul className={styles.menuContainer}>
+          {menuItems.map((item, index) => (
+            <li key={index} onClick={item.onClick} className={styles.menuitem}>
+              {item.label}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
