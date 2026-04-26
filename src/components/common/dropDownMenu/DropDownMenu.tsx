220c7bab8ddac292a783318b84dd437d8de5a99b
JuHeonParkk <ju10913@naver.com>
2026-04-26 17:54:15 +0900

    feat: trigger button 공통 컴포넌트 안으로 이동하여 관리

--- src/components/common/dropDownMenu/DropDownMenu.tsx
+++ src/components/common/dropDownMenu/DropDownMenu.tsx
@@ -2,40 +2,54 @@
 
 import { useState, useRef, useEffect } from 'react';
 
-import MoreIcon from '@/assets/icons/MoreIcon';
-import SettingIcon from '@/assets/icons/SettingIcon';
-
 import styles from './DropdownMenu.module.css';
 
-interface dropDownMenuProps {
-  status: 'setting' | 'kebab';
-  menuItems: Array<{
-    label: string;
-    onClick: () => void;
-  }>;
+interface MenuItem {
+  icon?: React.ComponentType<{ className?: string; color?: string }>;
+  color?: string;
+  label: string;
+  onClick: () => void;
 }
 
-const ICON_MAP = {
-  setting: SettingIcon,
-  kebab: MoreIcon,
-};
+interface DropdownMenuProps {
+  children?: React.ReactNode;
+  menuItems: MenuItem[];
+}
 
-export default function DropdownMenu({ status, menuItems }: dropDownMenuProps) {
+export default function DropdownMenu({ children, menuItems, ...props }: DropdownMenuProps) {
   const [isOpen, setIsOpen] = useState(false);
   const menuRef = useRef<HTMLDivElement>(null);
 
-  const Icon = ICON_MAP[status];
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(e.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+    if (isOpen) document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, [isOpen, setIsOpen]);
 
   return (
     <div className={styles.container} ref={menuRef}>
-      <button type="button" className={styles.iconButton} onClick={() => setIsOpen(!isOpen)}>
-        <Icon />
+      <button className={styles.triggerButton} onClick={() => setIsOpen(!isOpen)}>
+        {children}
       </button>
       {isOpen && (
         <ul className={styles.menuContainer}>
-          {menuItems.map((item, index) => (
-            <li key={index} onClick={item.onClick} className={styles.menuitem}>
-              {item.label}
+          {menuItems.map((item) => (
+            <li
+              key={item.label}
+              onClick={(e) => {
+                e.stopPropagation();
+                item.onClick();
+                setIsOpen(false);
+              }}
+              className={styles.menuItem}
+              style={{ color: item.color }}
+            >
+              {item.icon && <item.icon className={styles.menuIcon} color={item.color} />}
+              <span>{item.label}</span>
             </li>
           ))}
         </ul>
