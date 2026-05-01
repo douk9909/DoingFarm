4973f3ebfd5aca6857b67d9b040cb4778aa39e88
JuHeonParkk <ju10913@naver.com>
2026-05-01 21:13:09 +0900

    refactor: 함수 호출 방식 변경

--- src/lib/utils/toast.tsx
+++ src/lib/utils/toast.tsx
@@ -1,45 +1,55 @@
-// utils/toast.ts
 import toast, { ToastOptions } from 'react-hot-toast';
 
 import ToastSuccessIcon from '@/assets/icons/ToastSuccessIcon';
 import ToastErrorIcon from '@/assets/icons/ToastErrorIcon';
 
-const DEFAULT_OPTIONS: ToastOptions = {
-  duration: 3000,
-  className: 'custom-toast',
-  style: {
-    borderRadius: '8px',
-    fontSize: '14px',
-    padding: '12px 16px',
-    display: 'flex',
-    alignItems: 'center',
-    gap: '8px',
-  },
+const baseStyle = {
+  borderRadius: '8px',
+  fontSize: '14px',
+  padding: '12px 16px',
+  display: 'flex',
+  alignItems: 'center',
+  gap: '8px',
 };
 
-type ToastType = 'success' | 'error';
+const DEFAULT_OPTIONS: ToastOptions = {
+  duration: 3000,
+  id: 'unique-toast',
+  className: 'custom-toast',
+  style: baseStyle,
+};
 
-interface ShowToastParams {
-  message: string;
-  type?: ToastType;
-  duration?: number;
-}
+type OpenToast = {
+  (message: string): string;
+  success: (message: string) => string;
+  error: (message: string) => string;
+};
 
-export const showToast = ({ message, type = 'success', duration }: ShowToastParams) => {
-  const options: ToastOptions = {
+export const showToast = ((message: string) => {
+  return toast(message, {
     ...DEFAULT_OPTIONS,
-    duration: duration ?? DEFAULT_OPTIONS.duration,
-  };
+    icon: null,
+  });
+}) as OpenToast;
 
-  const icon = type === 'success' ? <ToastSuccessIcon /> : <ToastErrorIcon />;
+showToast.success = (message: string) => {
+  return toast.success(message, {
+    ...DEFAULT_OPTIONS,
+    icon: <ToastSuccessIcon />,
+    style: {
+      ...baseStyle,
+      background: 'rgba(60, 179, 113, 1)',
+    },
+  });
+};
 
-  toast.custom(
-    (t) => (
-      <div className={`custom-toast ${t.visible ? 'enter' : 'leave'}`}>
-        {icon}
-        <span>{message}</span>
-      </div>
-    ),
-    options,
-  );
+showToast.error = (message: string) => {
+  return toast.error(message, {
+    ...DEFAULT_OPTIONS,
+    icon: <ToastErrorIcon />,
+    style: {
+      ...baseStyle,
+      background: 'rgba(255, 59, 48, 1)',
+    },
+  });
 };
