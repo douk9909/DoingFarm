e799eee7abf3feb76b002cbbbc143848e117149f
JuHeonParkk <ju10913@naver.com>
2026-05-05 02:10:08 +0900

    feat: 로딩스피너 제작

--- src/app/(with-nav)/dashboard/[dashboardId]/components/Skeleton/SkeletonCard.tsx
+++ src/app/(with-nav)/dashboard/[dashboardId]/components/Skeleton/SkeletonCard.tsx
@@ -5,22 +5,20 @@ export default function SkeletonCard() {
   const skeleton = base.skeletonBase;
   return (
     <div className={styles.columnWrapper}>
-      {[...Array(2)].map((_, i) => (
-        <div key={i} className={`${skeleton} ${styles.cardWrapper}`}>
-          <div className={`${skeleton} ${styles.image}`} />
-          <div className={`${skeleton} ${styles.title}`} />
-          <div className={styles.tags}>
-            {[...Array(2)].map((_, i) => (
-              <div key={i} className={`${skeleton} ${styles.tag}`} />
-            ))}
-          </div>
-          <div className={`${skeleton} ${styles.date}`} />
-          <div className={styles.userWrapper}>
-            <div className={`${skeleton} ${base.circle} ${styles.avatar}`} />
-            <div className={`${skeleton} ${styles.userName}`} />
-          </div>
+      <div className={`${skeleton} ${styles.cardWrapper}`}>
+        <div className={`${skeleton} ${styles.image}`} />
+        <div className={`${skeleton} ${styles.title}`} />
+        <div className={styles.tags}>
+          {[...Array(2)].map((_, i) => (
+            <div key={i} className={`${skeleton} ${styles.tag}`} />
+          ))}
         </div>
-      ))}
+        <div className={`${skeleton} ${styles.date}`} />
+        <div className={styles.userWrapper}>
+          <div className={`${skeleton} ${base.circle} ${styles.avatar}`} />
+          <div className={`${skeleton} ${styles.userName}`} />
+        </div>
+      </div>
     </div>
   );
 }
